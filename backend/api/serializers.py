0ca6ade314ab754ac45bb91062033fb89454189e
Guilherme Rodrigues <menmaotsutsuki@gmail.com>
2026-05-06 10:43:13 -0300

    BD Funcionando

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,14 +4,33 @@ from .models import Cliente, Equipamento, OrdemServico
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
-        fields = ['id', 'name', 'phone', 'email', 'address', 'createdAt']
+        fields = '__all__'
 
 class EquipamentoSerializer(serializers.ModelSerializer):
+    # Faz o mapeamento de clientId (Front) para owner (Back)
+    clientId = serializers.PrimaryKeyRelatedField(
+        source='owner', queryset=Cliente.objects.all()
+    )
+    clientName = serializers.ReadOnlyField(source='owner.name')
+
     class Meta:
         model = Equipamento
-        fields = ['id', 'description', 'brand', 'model', 'serialNumber', 'owner', 'createdAt']
+        fields = ['id', 'name', 'brand', 'model', 'serialNumber', 'clientId', 'clientName', 'createdAt']
 
 class OrdemServicoSerializer(serializers.ModelSerializer):
+    # Mapeamentos para as chaves do Front
+    clientId = serializers.PrimaryKeyRelatedField(
+        source='client', queryset=Cliente.objects.all()
+    )
+    clientName = serializers.ReadOnlyField(source='client.name')
+    equipmentId = serializers.PrimaryKeyRelatedField(
+        source='equipment', queryset=Equipamento.objects.all()
+    )
+    equipmentName = serializers.ReadOnlyField(source='equipment.name')
+
     class Meta:
         model = OrdemServico
-        fields = ['id', 'description', 'status', 'price', 'client', 'equipment', 'createdAt']
+        fields = [
+            'id', 'clientId', 'clientName', 'equipmentId', 'equipmentName', 
+            'problemDescription', 'status', 'price', 'createdAt', 'updatedAt'
+        ]
